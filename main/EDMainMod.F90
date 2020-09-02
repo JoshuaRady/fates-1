92fdf8aaef4c461c674f613f0b3d42b5e15bec0d
Joshua Rady <joshuarady@Steppin-Razor.local>
2020-09-01 21:51:54 -0400

    Add missing use statement.

--- main/EDMainMod.F90
+++ main/EDMainMod.F90
@@ -86,6 +86,8 @@ module EDMainMod
   use PRTLossFluxesMod,       only : PRTReproRelease
 
   use EDPftvarcon,            only : EDPftvarcon_inst
+  
+  use FatesVegetationManagementMod, only : managed_fecundity
 
   ! CIME Globals
   use shr_log_mod         , only : errMsg => shr_log_errMsg
