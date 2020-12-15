4f1c711384f381ca1375a70f0564780825e39b82
Joshua Rady <joshuarady@Steppin-Razor.local>
2020-12-15 11:09:34 -0500

    Correct bad constant name.

--- biogeochem/FatesVegetationManagementMod.F90
+++ biogeochem/FatesVegetationManagementMod.F90
@@ -3812,7 +3812,7 @@ contains
 !             harvest_patch(patch = best_patch, pfts = pfts, dbh_min = the_dbh_min, &
 !                           dbh_max = the_dbh_max, ht_min = the_ht_min, ht_max = the_ht_max, &
 !                           fraction = patch_harvest_fraction)
-            call kill_patch(patch = best_patch, flux_profile = vm_mort_bole_harvest, pfts = pfts, &
+            call kill_patch(patch = best_patch, flux_profile = bole_harvest, pfts = pfts, &
                             dbh_min = the_dbh_min, dbh_max = the_dbh_max, &
                             ht_min = the_ht_min, ht_max = the_ht_max, &
                             kill_fraction = patch_harvest_fraction) ! ??????
